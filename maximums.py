7388c68c14809217c3d0cc17e40645454166818f
EliseoAlvarezB <ealvarezbrescia@mail.austral.edu.ar>
2025-09-14 22:23:02 -0300

    Add files via upload

--- maximums.py
+++ maximums.py
@@ -1,17 +1,8 @@
-
 def max_of_two(x, y):
-    if x > y:
-        print(x)
-    else:
-        print(y)
-# max_of_two(5,4)
+    mayor = max(x, y)
+    return mayor
 
 
 def max_of_three(x, y, z):
-    if x > y and x > z:
-        print(x)
-    elif y > x and y > z:
-        print(y)
-    else: 
-        print(z)
-# max_of_three(5,4,7)
+    mayor = max(x, y, z)
+    return mayor
